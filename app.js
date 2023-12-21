f34d9d7c7df6994d13c78e18f838fd896ddaac59
Meganus <mohitjassal157@gmail.com>
2023-12-21 05:34:12 -0800

    new patch

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(cookiParser());
 
 
 app.use(cors({
-    origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL2],
+    origin: "https://mobile-delivery-frontend.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
 }));
